a6ce96f614f5146c65d3635530c6e66c235c78a0
Kamal <kamal@eidolonsystems.com>
2019-08-22 11:14:41 -0400

    Added check for NotImplemented when performing special operators on Python objects.

--- Python/Source/Object.cpp
+++ Python/Source/Object.cpp
@@ -3,13 +3,25 @@
 using namespace pybind11;
 
 object pybind11::operator +(const object& left, const object& right) {
-  return left.attr("__add__")(right);
+  auto r = left.attr("__add__")(right);
+  if(r.ptr() == Py_NotImplemented) {
+    return right.attr("__radd__")(left);
+  }
+  return r;
 }
 
 bool pybind11::operator <(const object& left, const object& right) {
-  return left.attr("__lt__")(right).cast<bool>();
+  auto r = left.attr("__lt__")(right);
+  if(r.ptr() == Py_NotImplemented) {
+    return right.attr("__ge__")(left).cast<bool>();
+  }
+  return r.cast<bool>();
 }
 
 bool pybind11::operator >=(const object& left, const object& right) {
-  return left.attr("__ge__")(right).cast<bool>();
+  auto r = left.attr("__ge__")(right);
+  if(r.ptr() == Py_NotImplemented) {
+    return right.attr("__lt__")(left).cast<bool>();
+  }
+  return r.cast<bool>();
 }
