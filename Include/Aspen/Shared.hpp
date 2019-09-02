be2b0c886c3f9d6ab7d8f9f18d5006a6d45f24a9
Kamal Mansouri <kamal@eidolonsystems.com>
2019-09-02 00:57:14 -0400

    Fixed a bug when copying a Shared reactor that had been previously evaluated.

--- Include/Aspen/Shared.hpp
+++ Include/Aspen/Shared.hpp
@@ -20,10 +20,8 @@ namespace Aspen {
 
       /**
        * Constructs a Shared reactor.
-       * @param args The arguments used to emplace the shared reactor.
        */
-      template<typename... A>
-      explicit Shared(A&&... args);
+      Shared();
 
       /**
        * Constructs a Shared reactor.
@@ -32,6 +30,14 @@ namespace Aspen {
       template<typename A, typename = std::enable_if_t<
         !std::is_base_of_v<Shared, std::decay_t<A>>>>
       explicit Shared(A&& args);
+
+      /**
+       * Constructs a Shared reactor.
+       * @param args The arguments used to emplace the shared reactor.
+       */
+      template<typename A, typename... B, typename = std::enable_if_t<
+        !std::is_base_of_v<Shared, std::decay_t<A>>>>
+      explicit Shared(A&& arg, B&&... args);
 
       Shared(const Shared& shared);
 
@@ -78,15 +84,21 @@ namespace Aspen {
       m_sequence(-1) {}
 
   template<typename R>
-  template<typename... A>
-  Shared<R>::Shared(A&&... args)
-    : m_entry(std::make_shared<Entry>(std::forward<A>(args)...)),
+  Shared<R>::Shared()
+    : m_entry(std::make_shared<Entry>()),
       m_is_empty(true) {}
 
   template<typename R>
   template<typename A, typename>
   Shared<R>::Shared(A&& args)
     : m_entry(std::make_shared<Entry>(std::forward<A>(args))),
+      m_is_empty(true) {}
+
+  template<typename R>
+  template<typename A, typename... B, typename>
+  Shared<R>::Shared(A&& arg, B&&... args)
+    : m_entry(std::make_shared<Entry>(std::forward<A>(arg),
+        std::forward<B>(args)...)),
       m_is_empty(true) {}
 
   template<typename R>
