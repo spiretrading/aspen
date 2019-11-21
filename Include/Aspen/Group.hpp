f6749353520cd209adac438bddb3443194709689
Kamal <kamal@eidolonsystems.com>
2019-11-21 17:56:31 -0500

    Added two unit tests to fix crashes in the Group reactor.

--- Include/Aspen/Group.hpp
+++ Include/Aspen/Group.hpp
@@ -41,8 +41,11 @@ namespace Aspen {
         Child(U&& reactor);
       };
       std::optional<T> m_producer;
-      std::list<Child> m_children;
+      std::unique_ptr<std::list<Child>> m_children;
+      typename std::list<Child>::iterator m_current;
       typename std::list<Child>::iterator m_position;
+
+      void increment();
   };
 
   template<typename T, typename = std::enable_if_t<
@@ -68,7 +71,8 @@ namespace Aspen {
   template<typename TF, typename>
   Group<T>::Group(TF&& producer)
     : m_producer(std::forward<TF>(producer)),
-      m_position(m_children.end()) {}
+      m_children(std::make_unique<std::list<Child>>()),
+      m_position(m_children->end()) {}
 
   template<typename T>
   State Group<T>::commit(int sequence) noexcept {
@@ -77,7 +81,10 @@ namespace Aspen {
         auto producer_state = m_producer->commit(sequence);
         if(has_evaluation(producer_state)) {
           try {
-            m_children.emplace_back(m_producer->eval());
+            m_children->emplace_back(m_producer->eval());
+            if(m_children->size() == 1) {
+              m_position = m_children->begin();
+            }
           } catch(...) {}
         }
         if(has_continuation(producer_state)) {
@@ -85,51 +92,51 @@ namespace Aspen {
         }
         if(is_complete(producer_state)) {
           m_producer = std::nullopt;
-          if(m_children.empty() ||
-              m_children.size() == 1 && m_children.front().m_is_complete) {
-            return State::COMPLETE;
-          }
         }
       }
       return State::NONE;
     }();
-    auto start = [&] {
-      if(m_position == m_children.end() ||
-          std::next(m_position) == m_children.end()) {
-        return m_children.begin();
-      }
-      return std::next(m_position);
-    }();
-    auto looped = false;
-    for(auto i = start; !looped || i != start;) {
-      auto& child = *i;
-      if(child.m_is_complete) {
-        i = m_children.erase(i);
-      } else {
-        auto child_state = child.m_reactor.commit(sequence);
-        if(has_continuation(child_state)) {
-          state = combine(state, State::CONTINUE);
-        } else if(is_complete(child_state)) {
-          child.m_is_complete = true;
-          if(m_children.size() == 1 && !m_producer.has_value()) {
-            state = combine(state, State::COMPLETE);
+    if(!m_children->empty()) {
+      auto start = m_position;
+      while(true) {
+        auto& child = *m_position;
+        if(child.m_is_complete) {
+          if(m_position != m_current) {
+            m_position = m_children->erase(m_position);
+            if(m_position == m_children->end()) {
+              m_position = m_children->begin();
+            }
+          } else {
+            increment();
           }
-        }
-        if(has_evaluation(child_state)) {
-          state = combine(state, State::EVALUATED);
-          if(m_children.size() > 1) {
-            state = combine(state, State::CONTINUE);
-          }
-          m_position = i;
-          break;
         } else {
-          ++i;
+          auto child_state = child.m_reactor.commit(sequence);
+          if(has_continuation(child_state)) {
+            state = combine(state, State::CONTINUE);
+          } else if(is_complete(child_state)) {
+            child.m_is_complete = true;
+          }
+          if(has_evaluation(child_state)) {
+            state = combine(state, State::EVALUATED);
+            if(m_children->size() > 1) {
+              state = combine(state, State::CONTINUE);
+            }
+            m_current = m_position;
+            increment();
+            break;
+          } else {
+            increment();
+          }
+        }
+        if(m_position == start) {
+          break;
         }
       }
-      if(i == m_children.end()) {
-        looped = true;
-        i = m_children.begin();
-      }
+    }
+    if((m_children->empty() ||
+        m_children->size() == 1 && m_children->front().m_is_complete) &&
+        !m_producer.has_value()) {
+      state = combine(state, State::COMPLETE);
     }
     return state;
   }
@@ -137,7 +144,15 @@ namespace Aspen {
   template<typename T>
   eval_result_t<typename Group<T>::Type> Group<T>::eval()
       const noexcept(is_noexcept) {
-    return m_position->m_reactor.eval();
+    return m_current->m_reactor.eval();
+  }
+
+  template<typename T>
+  void Group<T>::increment() {
+    ++m_position;
+    if(m_position == m_children->end()) {
+      m_position = m_children->begin();
+    }
   }
 }
 
