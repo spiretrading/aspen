b85c108d39f7096b42889fbf4a9e2128fad41a40
Kamal <kamal@eidolonsystems.com>
2019-12-26 14:12:17 -0500

    Updated path to include/source files.

--- Config/dependencies.cmake
+++ Config/dependencies.cmake
@@ -3,8 +3,8 @@ if(WIN32)
 else()
   include("${CMAKE_CURRENT_LIST_DIR}/dependencies.posix.cmake")
 endif()
-set(ASPEN_INCLUDE_PATH "${PROJECT_BINARY_DIR}/Include")
-set(ASPEN_SOURCE_PATH "${PROJECT_BINARY_DIR}/Source")
+set(ASPEN_INCLUDE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Include")
+set(ASPEN_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Source")
 set(CATCH2_INCLUDE_PATH
   "${PROJECT_BINARY_DIR}/Dependencies/Catch2-2.6.1/single_include")
 set(PYBIND11_INCLUDE_PATH
