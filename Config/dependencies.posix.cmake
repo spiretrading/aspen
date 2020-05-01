f0baa278ba8e4c7b14cb83e1063e1a71b1ef25e7
Kamal <kamal@eidolonsystems.com>
2020-05-01 12:02:13 -0400

    Updated Python dependency.

--- Config/dependencies.posix.cmake
+++ Config/dependencies.posix.cmake
@@ -1,6 +1,6 @@
 set(PYTHON_INCLUDE_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/include/python3.8m")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/include/python3.8")
 set(PYTHON_LIBRARY_DEBUG_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8m.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8.a")
 set(PYTHON_LIBRARY_OPTIMIZED_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8m.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8.a")
