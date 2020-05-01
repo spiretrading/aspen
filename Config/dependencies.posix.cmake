4d1fce64ba2e036f810332828ebb9796596c5468
Kamal Mansouri <kamal@eidolonsystems.com>
2020-04-30 23:13:17 -0400

    Updated paths.

--- Config/dependencies.posix.cmake
+++ Config/dependencies.posix.cmake
@@ -1,6 +1,6 @@
 set(PYTHON_INCLUDE_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.6.7/include/python3.6m")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/include/python3.8m")
 set(PYTHON_LIBRARY_DEBUG_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.6.7/lib/libpython3.6m.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8m.a")
 set(PYTHON_LIBRARY_OPTIMIZED_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.6.7/lib/libpython3.6m.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/Python-3.8.2/lib/libpython3.8m.a")
