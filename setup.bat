92cc264e2b5b116a2c2d8bc43b7975cbf6303181
Kamal <kamal@eidolonsystems.com>
2023-01-13 18:09:51 -0500

    Updated Windows setup script to use latest dependencies.

--- setup.bat
+++ setup.bat
@@ -19,29 +19,29 @@ FOR /f "usebackq delims=" %%i IN (`!VSWHERE! -prerelease -latest -property insta
     CALL "%%i\Common7\Tools\vsdevcmd.bat"
   )
 )
-IF NOT EXIST doctest-2.4.6 (
-  wget https://github.com/onqtam/doctest/archive/2.4.6.zip --no-check-certificate
+IF NOT EXIST doctest-2.4.9 (
+  wget https://github.com/doctest/doctest/archive/refs/tags/v2.4.9.zip --no-check-certificate
   IF !ERRORLEVEL! LEQ 0 (
-    tar -xf 2.4.6.zip
+    tar -xf 2.4.9.zip
   ) ELSE (
     SET EXIT_STATUS=1
   )
-  DEL /F /Q 2.4.6.zip
+  DEL /F /Q 2.4.9.zip
 )
-IF NOT EXIST pybind11-2.6.1 (
-  wget https://github.com/pybind/pybind11/archive/v2.6.1.zip -O pybind11-2.6.1.zip --no-check-certificate
+IF NOT EXIST pybind11-2.10.3 (
+  wget https://github.com/pybind/pybind11/archive/refs/tags/v2.10.3.zip -O pybind11-2.10.3.zip --no-check-certificate
   IF !ERRORLEVEL! LEQ 0 (
-    tar -xf pybind11-2.6.1.zip
+    tar -xf pybind11-2.10.3.zip
   ) ELSE (
     SET EXIT_STATUS=1
   )
-  DEL /F /Q pybind11-2.6.1.zip
+  DEL /F /Q pybind11-2.10.3.zip
 )
-IF NOT EXIST Python-3.9.1 (
-  wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz --no-check-certificate
+IF NOT EXIST Python-3.11.1 (
+  wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz --no-check-certificate
   IF !ERRORLEVEL! LEQ 0 (
-    gzip -d -c Python-3.9.1.tgz | tar -xf -
-    PUSHD Python-3.9.1
+    gzip -d -c Python-3.11.1.tgz | tar -xf -
+    PUSHD Python-3.11.1
     PUSHD PCbuild
     CALL build.bat -E -c Debug -p Win32
     CALL build.bat -E -c Release -p Win32
@@ -51,7 +51,7 @@ IF NOT EXIST Python-3.9.1 (
   ) ELSE (
     SET EXIT_STATUS=1
   )
-  DEL /F /Q Python-3.9.1.tgz
+  DEL /F /Q Python-3.11.1.tgz
 )
 IF NOT EXIST cache_files (
   MD cache_files
