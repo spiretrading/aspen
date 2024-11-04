baa1acb9ea7a7dd780b932929e49591ac358066e
Kamal <kamal@eidolonsystems.com>
2024-11-04 11:13:47 -0500

    The Python install scripts will make the user-site directories if they don't yet exist.

--- install_python.bat
+++ install_python.bat
@@ -4,4 +4,5 @@ IF "%1" == "" (
   SET CONFIG=%1
 )
 FOR /f "delims=" %%i IN ('python -m site --user-site') DO SET PYTHON_PATH="%%i"
+mkdir "%PYTHON_PATH%" /p
 COPY "Libraries\%CONFIG%\aspen.pyd" "%PYTHON_PATH%"
--- install_python.sh
+++ install_python.sh
@@ -5,4 +5,5 @@ else
   config="$1"
 fi
 python_directory=$(python3 -m site --user-site)
+mkdir -p "$python_directory"
 cp "Libraries/$config/aspen.so" "$python_directory"
