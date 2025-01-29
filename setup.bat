e8f00acd0ae0c79f49f05fa18c5a2785219f0a4f
Kamal Mansouri <kamal@eidolonsystems.com>
2025-01-28 21:27:08 -0500

    Removed dependency on GNU utilities from the Windows build scripts.

--- setup.bat
+++ setup.bat
@@ -3,44 +3,55 @@ SETLOCAL EnableDelayedExpansion
 SET EXIT_STATUS=0
 SET ROOT=%cd%
 IF EXIST cache_files\aspen.txt (
-  FOR /F %%i IN (
-      'ls -l --time-style=full-iso "%~dp0\setup.bat" ^| awk "{print $6 $7}"') DO (
-    FOR /F %%j IN (
-        'ls -l --time-style=full-iso cache_files\aspen.txt ^| awk "{print $6 $7}"') DO (
-      IF "%%i" LSS "%%j" (
-        EXIT /B 0
-      )
-    )
+  powershell -Command "& { "^
+    "$setupTimestamp = (Get-Item '%~dp0setup.bat').LastWriteTime; "^
+    "$aspenTimestamp = (Get-Item 'cache_files\\aspen.txt').LastWriteTime; "^
+    "if ($setupTimestamp -lt $aspenTimestamp) {"^
+    "  Exit 0;"^
+    "} else {"^
+    "  Exit 1;"^
+    "}"^
+  "}"
+  IF ERRORLEVEL 0 (
+    EXIT /B 0
   )
 )
 SET VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-FOR /f "usebackq delims=" %%i IN (`!VSWHERE! -prerelease -latest -property installationPath`) DO (
+FOR /F "usebackq delims=" %%i IN (` ^
+    !VSWHERE! -prerelease -latest -property installationPath`) DO (
   IF EXIST "%%i\Common7\Tools\vsdevcmd.bat" (
     CALL "%%i\Common7\Tools\vsdevcmd.bat"
   )
 )
 IF NOT EXIST doctest-2.4.11 (
-  wget https://github.com/doctest/doctest/archive/refs/tags/v2.4.11.zip --no-check-certificate
+  powershell -Command "Invoke-WebRequest "^
+    "-Uri https://github.com/doctest/doctest/archive/refs/tags/v2.4.11.zip "^
+    "-OutFile v2.4.11.zip"
   IF !ERRORLEVEL! LEQ 0 (
-    tar -xf v2.4.11.zip
+    powershell -Command "Expand-Archive -Path v2.4.11.zip -DestinationPath ."
   ) ELSE (
     SET EXIT_STATUS=1
   )
   DEL /F /Q v2.4.11.zip
 )
 IF NOT EXIST pybind11-2.13.6 (
-  wget https://github.com/pybind/pybind11/archive/refs/tags/v2.13.6.zip -O pybind11-2.13.6.zip --no-check-certificate
+  powershell -Command "Invoke-WebRequest "^
+    "-Uri https://github.com/pybind/pybind11/archive/refs/tags/v2.13.6.zip "^
+    "-OutFile pybind11-2.13.6.zip"
   IF !ERRORLEVEL! LEQ 0 (
-    tar -xf pybind11-2.13.6.zip
+    powershell ^
+      -Command "Expand-Archive -Path pybind11-2.13.6.zip -DestinationPath ."
   ) ELSE (
     SET EXIT_STATUS=1
   )
   DEL /F /Q pybind11-2.13.6.zip
 )
 IF NOT EXIST Python-3.13.0 (
-  wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz --no-check-certificate
+  powershell -Command "Invoke-WebRequest "^
+    "-Uri https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz "^
+    "-OutFile Python-3.13.0.tgz"
   IF !ERRORLEVEL! LEQ 0 (
-    gzip -d -c Python-3.13.0.tgz | tar -xf -
+    powershell -Command "tar -xf Python-3.13.0.tgz"
     PUSHD Python-3.13.0
     PUSHD PCbuild
     CALL build.bat -c Debug
