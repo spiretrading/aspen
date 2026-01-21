a5e0966f63e3d695aeb943297a950d2c8f3c3a8c
Kamal <kamal@spiretrading.com>
2026-01-21 17:03:19 -0500

    Fix trailing space in stub file creation
    Use >file ECHO syntax instead of ECHO > file to prevent
    trailing spaces from being written to build.bat and
    configure.bat stub files.

--- configure.bat
+++ configure.bat
@@ -2,12 +2,12 @@
 SETLOCAL EnableDelayedExpansion
 SET ROOT=%cd%
 IF NOT EXIST build.bat (
-  ECHO @ECHO OFF > build.bat
-  ECHO CALL "%~dp0build.bat" %%* >> build.bat
+  >build.bat ECHO @ECHO OFF
+  >>build.bat ECHO CALL "%~dp0build.bat" %%*
 )
 IF NOT EXIST configure.bat (
-  ECHO @ECHO OFF > configure.bat
-  ECHO CALL "%~dp0configure.bat" %%* >> configure.bat
+  >configure.bat ECHO @ECHO OFF
+  >>configure.bat ECHO CALL "%~dp0configure.bat" %%*
 )
 SET DIRECTORY=%~dp0
 SET DEPENDENCIES=
